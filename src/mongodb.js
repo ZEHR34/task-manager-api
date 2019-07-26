945b46064a9f9d3cd130ac260b338e78e1ca6d28
muhammad saad <mo3dysaad@gmail.co>
2019-07-27 01:41:24 +0200

    project tamplate

---
+++ src/mongodb.js
@@ -0,0 +1,7 @@
+const momgodb = require('mongodb')
+const MongoClient = momgodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true })
