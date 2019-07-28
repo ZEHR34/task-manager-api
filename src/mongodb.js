5384d3cdb79fc48f17d9159480f87b3880e86100
muhammad saad <mo3dysaad@gmail.co>
2019-07-29 00:16:44 +0200

    mongodb docs

--- src/mongodb.js
+++ src/mongodb.js
@@ -4,4 +4,58 @@ const MongoClient = momgodb.MongoClient
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true })
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+  if (error) {
+    return console.log('unable to connect to database!')
+  }
+
+  console.log('connected correctly!')
+  const db = client.db(databaseName)
+  db.collection('tasks').insertMany([
+    {
+      description: 'clean the room',
+      completed: false
+    }, {
+      description: 'clean the shelf',
+      completed: false
+    }, {
+      description: 'clean the class',
+      completed: false
+    }, {
+      description: 'clean the office',
+      completed: false
+    }
+  ], (error, result) => {
+    if (error) {
+      return console.log('unable to insert document')
+    }
+    console.log(result.ops)
+  })
+  /**
+  db.collection('users').insertMany([
+    {
+      name: 'ma', age: 25
+    },
+    {
+      name: 'mo', age: 15
+    }
+  ], (error, result) => {
+    if (error) {
+      return console.log('unable to insert user')
+    }
+    console.log(result.ops)
+  })
+        db.collection('users').insertOne({
+       name: 'ms', age: 22
+     }, (error, result) => {
+       if (error) {
+         return console.log('unable to insert user')
+       }
+       console.log(result.ops)
+   
+     })
+   
+  */
+
+
+})
